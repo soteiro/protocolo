00f4133b798677a713843420bea6d68498035b4f
soteiro <diego_sarq@hotmail.com>
2025-06-14 21:07:26 -0400

    Add Netlify configuration for build and publish settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
